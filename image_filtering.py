4ea69fc57a436637343456fb2146e9f1873cde89
SubhashRelangi <subhashrelangi@gmail.com>
2025-12-06 14:58:26 +0530

    Learned Image Bluring in OpenCV using different Methods

--- image_filtering.py
+++ image_filtering.py
@@ -6,18 +6,21 @@ image = cv2.imread("images/url0.png")
 if image is None:
     print("Could not read image.")
 
-kernell = np.array([[1,0,0],
-                    [0,1,0],
-                    [0,0,1]])
+# APPLYING THE IDENTITY MATRIX EFFECT TO THE IMAGE USING THE FILTER2D() METHOD
+# kernell = np.array([[0,0,0],
+#                     [0,1,0],
+#                     [0,0,0]])
 
-identity = cv2.filter2D(src=image, ddepth=-1, kernel=kernell)
+# identity = cv2.filter2D(src=image, ddepth=-1, kernel=kernell)
 
-cv2.imshow("Original Image", image)
-cv2.imshow("Identity Image", identity)
-cv2.imwrite("IdentityImage.png", identity)
+# cv2.imshow("Original Image", image)
+# cv2.imshow("Identity Image", identity)
+# cv2.imwrite("IdentityImage.png", identity)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+# APPLYING THE BLUR MATRIX EFFECT TO THE IMAGE USING THE FILTER2D() METHOD
 
 # kernel2 = np.ones((5,5), np.float32) / 25
 
@@ -28,3 +31,35 @@ cv2.destroyAllWindows()
 
 # cv2.waitKey(0)
 # cv2.imwrite("Blur_Kernel.png", img)
+
+#APPLYING BLUR  EFFECT TO THE IMAGE USING BLUR() METHOD
+
+# img_blur = cv2.blur(src=image, ksize=(5,5))
+
+# cv2.imshow("Original Image", image)
+# cv2.imshow("Blur Image", img_blur)
+
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+#APPLYING BLUR EFFECT USING GaussianBlur() METHOD
+
+# gaussian_blur = cv2.GaussianBlur(src=image, ksize=(5,5), sigmaX=0, sigmaY=0)
+
+# cv2.imshow("Original Image", image)
+# cv2.imshow("GaussianBlur", gaussian_blur)
+
+# cv2.imwrite("images/GaussianBlur.png", gaussian_blur)
+
+#APPLYING BLUR EFFECT USING MEdianBlur() METHOD
+
+median = cv2.medianBlur(src=image, ksize=5)
+ 
+cv2.imshow('Original', image)
+cv2.imshow('Median Blurred', median)
+     
+cv2.waitKey()
+cv2.imwrite('median_blur.jpg', median)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
